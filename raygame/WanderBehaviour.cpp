8ceb05ef1fcf13aea6e749837a0611e8a5162b1b
Britt <s208057@students.aie.edu>
2021-03-03 16:53:57 -0600

    Added Wander to behaviours

--- raygame/WanderBehaviour.cpp
+++ raygame/WanderBehaviour.cpp
@@ -1,1 +1,22 @@
 #include "WanderBehaviour.h"
+
+WanderBehaviour::WanderBehaviour()
+{
+	m_target = nullptr;
+	circleRadius = 5;
+	m_wanderForce = 1;
+}
+
+WanderBehaviour::WanderBehaviour(Agent* Target, float Radius, float WanderForces)
+{
+	m_target = Target;
+	circleRadius = Radius;
+	m_wanderForce = WanderForces;
+}
+
+void WanderBehaviour::setAngle()
+{
+
+
+
+}
--- raygame/WanderBehaviour.h
+++ raygame/WanderBehaviour.h
@@ -1,15 +1,25 @@
 #pragma once
 #include "SteeringBehaviour.h"
+
 class WanderBehaviour :
 	public SteeringBehaviour
 {
 public:
 	WanderBehaviour();
 
-	WanderBehaviour(float CircleRadius);
+	WanderBehaviour(Agent* Target, float Radius, float WanderForces);
+
+	void setAngle();
+
+	MathLibrary::Vector2 calculateForce(Agent* Target) override;
+	
+	void update(float deltatime);
 
 private:
 	float circleRadius;
 
+	Agent* m_target;
+
+	float m_wanderForce;
 };
 
